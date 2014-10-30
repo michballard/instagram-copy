71c072f7405429a5f373c7ec10b83050cd26e5e0
michballard <michballard@gmail.com>
2014-10-30 10:15:47 +0000

    Background CSS changes, updated Readme

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # INSTAGRAMME
 
-This project is designed to replicate Instagram using Ruby on Rails.  This was done while studying with Makers Academy. 
+This project is designed to replicate the website Instagram, with the main purpose to use Ruby on Rails.  This was done in week 9 of studying with Makers Academy. 
 
 <insert image>
 
 
+## Features
+
+- Sign up, sign in and sign out using Devise
+- Add a post with a picture, optionally including comments and tags
+- View all posts created
+- View related posts by clicking tags
+- A post can be edited or deleted by the signed in post author
+
+
+## My key learnings
+
+- Using Rails I was able to learn about how quickly a website can be created
+- Using Bootstrap for styling I was able to understand the benefits for using it, but also learn about the limitations compared to styling with CSS from scratch
+
+
 ## Technologies used
 
-- Ruby on Rails
+- Ruby 
+- Rails
 - HTML
 - CSS
 - Bootstrap
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -3,86 +3,91 @@ require 'rails_helper'
 describe 'posts' do 
   
   context 'no posts have been added' do 
-  	it 'should display a prompt to add a post' do 
-  		visit '/posts'
-  		expect(page).to have_content "No posts yet"
-  		expect(page).to have_link "Add a post"
+    it 'should display a prompt to add a post' do 
+      visit '/posts'
+      expect(page).to have_content "No posts yet"
+      expect(page).to have_link "Add a post"
     end
   end 
 
   context 'posts have been added' do 
-  	before do 
-  		Post.create(title: 'Here is a picture')
-  	end
-  	it 'should display them' do
-  		visit '/posts'
-  		expect(page).to have_content('Here is a picture')
-  		expect(page).not_to have_content('No posts yet')
-  	end
+    before do 
+      Post.create(title: 'Here is a picture')
+    end
+    it 'should display them' do
+      visit '/posts'
+      expect(page).to have_content('Here is a picture')
+      expect(page).not_to have_content('No posts yet')
+    end
   end
 
   context 'time created' do 
-  	before do
-  		Post.create(title: 'Here is a picture')
-  	end
-  	it 'is displayed with a post' do 
-  		visit '/posts'
-  		expect(page).to have_content('less than a minute ago')
-  	end
+    before do
+      Post.create(title: 'Here is a picture')
+    end
+    it 'is displayed with a post' do 
+      visit '/posts'
+      expect(page).to have_content('less than a minute ago')
+    end
   end
 
   describe 'creating posts' do 
     
     context 'logged out' do 
-    	it 'should prompt to sign in' do 
-    		visit '/posts'
-    		click_link 'Add a post'
-    		expect(page).to have_content 'Sign up'
-    	end
+      it 'should prompt to sign in' do 
+        visit '/posts'
+        click_link 'Add a post'
+        expect(page).to have_content 'Sign up'
+      end
     end
 
     context 'logged in' do 
-    	before do
-    		visit '/users/sign_up'
-    		fill_in 'user[email]', with: 'a@a.com'
-    		fill_in 'user[password]', with: '12345678'
-    		fill_in 'user[password_confirmation]', with: '12345678'
-    		click_button 'Sign up'
-    	end
+      before do
+        visit '/users/sign_up'
+        fill_in 'user[email]', with: 'a@a.com'
+        fill_in 'user[password]', with: '12345678'
+        fill_in 'user[password_confirmation]', with: '12345678'
+        click_button 'Sign up'
+      end
 
-	  	it 'prompts user to upload picture and fill in title, then displays a new post' do 
-	      visit '/posts'
-	      click_link 'Add a post'
-	      fill_in 'Title', with: 'Here is a picture'
-	      attach_file 'Image', Rails.root.join('spec/images/photo.jpg')
-	      click_button 'Create Post'
+      it 'prompts user to upload picture and fill in title, then displays a new post' do 
+        visit '/posts'
+        click_link 'Add a post'
+        fill_in 'Title', with: 'Here is a picture'
+        attach_file 'Image', Rails.root.join('spec/images/photo.jpg')
+        click_button 'Create Post'
         expect(current_path).to eq '/posts'
-	      expect(page).to have_content 'Here is a picture'
+        expect(page).to have_content 'Here is a picture'
         expect(page).to have_css 'img.post-image'
-	  	end 
+      end 
     end 
   end
 
   describe 'editing posts' do 
-  	before do
-  		michelle = User.create(email: 'a@a.com', password: '12345678', password_confirmation: '12345678')
-  	  login_as michelle 
-  	  Post.create(title: 'Here is a picture') 
-  	end
-  	it 'can allow a user to edit a post' do 
-	    visit '/posts'
-	    click_link 'Edit'
-	    fill_in 'Title', with: 'This is my picture'
-	    click_button 'Update Post'
-	    expect(page).to have_content 'This is my picture'
-	    expect(current_path).to eq '/posts'
-	  end
+    context 'a guest user' do 
+      
+    end 
+    context 'a signed in user' do 
+      before do
+        michelle = User.create(email: 'a@a.com', password: '12345678', password_confirmation: '12345678')
+        login_as michelle 
+        Post.create(title: 'Here is a picture') 
+      end
+      it 'can allow a user to edit a post' do 
+        visit '/posts'
+        click_link 'Edit'
+        fill_in 'Title', with: 'This is my picture'
+        click_button 'Update Post'
+        expect(page).to have_content 'This is my picture'
+        expect(current_path).to eq '/posts'
+      end
+    end 
   end
 
   describe 'deleting posts' do 
-  	before do 
-  		michelle = User.create(email: 'a@a.com', password: '12345678', password_confirmation: '12345678')
-  	  login_as michelle 
+    before do 
+      michelle = User.create(email: 'a@a.com', password: '12345678', password_confirmation: '12345678')
+      login_as michelle 
       Post.create(title: 'Here is a picture')
     end
     it 'removes a post when a user clicks a delete link' do 
